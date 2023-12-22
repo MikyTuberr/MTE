f8808c0cd39efe452a618e0e450f2b70d6ac74ae
Piotr Lachowicz <pio1342@gmail.com>
2023-12-22 14:12:26 +0100

    imguievents

--- MTE/src/MTE.h
+++ MTE/src/MTE.h
@@ -5,6 +5,7 @@
 #include "MTE/Application.h"
 #include "MTE/Layer.h"
 #include "MTE/Log.h"
+#include "MTE/ImGui/ImGuiLayer.h"
 
 // -----EntryPoint-----------------------
 #include "MTE/EntryPoint.h"
--- MTE/src/MTE/ImGui/ImGuiLayer.h
+++ MTE/src/MTE/ImGui/ImGuiLayer.h
@@ -2,9 +2,13 @@
 
 #include "MTE/Layer.h"
 
+#include "MTE/Events/ApplicationEvent.h"
+#include "MTE/Events/KeyEvent.h"
+#include "MTE/Events/MouseEvent.h"
+
 namespace MTE {
 
-	class ImGuiLayer : public Layer
+	class MTE_API ImGuiLayer : public Layer
 	{
 	public:
 		ImGuiLayer();
@@ -15,6 +19,16 @@ namespace MTE {
 		void OnUpdate();
 		void OnEvent(Event& event);
 	private:
-
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+	private:
+		bool m_BlockEvents = true;
+		float m_Time = 0.0f;
 	};
 }
